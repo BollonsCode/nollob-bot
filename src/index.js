c345f9ece0d982010dfa53990d75dd00be014895
geruso <victor.geruso@gmail.com>
2020-08-01 16:44:35 -0300

    [UPD] novos comandos basicos do bot

--- src/index.js
+++ src/index.js
@@ -1,16 +1,50 @@
-const discordJs = require("discord.js");
-const TOKEN = require("./config/token");
+// const ping = require("ping");
+const DiscordJs = require("discord.js");
+const conf = require("./config/token");
 
-const bot = new discordJs.Client();
+const bot = new DiscordJs.Client();
+const hook = new DiscordJs.WebhookClient(conf.ID_WH, conf.TOKEN_WH);
 
-bot.login(TOKEN);
+bot.login(conf.TOKEN_GERAL);
+
+const prefix = "$";
 
 bot.on("ready", () => {
   console.log("estou online no Discord!");
 });
 
 bot.on("message", (msg) => {
-  if (msg.content === "$hello") {
+  if (msg.content === `${prefix}hello`) {
     msg.reply("Olá, seja bem vindo ao nosso server!");
+  }
+  if (msg.content === `${prefix}help`) {
+    hook
+      .sendSlackMessage({
+        username: "Wumpus",
+        attachments: [
+          {
+            pretext:
+              "Aqui estão so meus comandos:" +
+              "\n\n`$help` -> uma empurrãozinho nos meus comandos" +
+              "\n\n`$hello` -> Mensagem de boas vindas" +
+              "\n\n`$ping` -> `Pong` eu testo o ping para você" +
+              "\n\n`$nv-data` 'dia e mes do seu aniversário' -> salvo o seu aniversário na minha cachola!" +
+              "\nExemplo: `$nv-data` 1 de maio" +
+              "\n\n`$ls-nv-mes` 'mes desejado' -> listo todos os aniversariantes do mês desejado ou do mes atual" +
+              "\nExemplo mes desejado: `$ls-nv-mes` 1" +
+              "\nExemplo mes atual: `$ls-nv-mes`",
+            color: "#007cdc",
+            ts: Date.now() / 1000,
+          },
+        ],
+      })
+      .catch(console.error);
+  }
+  if (msg.content === `${prefix}ping`) {
+    // let res = ping.sys.probe("google.com", {
+    //   timeout: 10,
+    //   extra: ["-i", "2"],
+    // });
+    // msg.channel.send(`Pong, ${res} ms!`);
   }
 });
